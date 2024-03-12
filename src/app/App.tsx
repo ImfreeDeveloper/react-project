d3bb8329d8b97673f38e7d8ad475f1fc0c88ac1a
a.galeev <a.galeev@malltech-lab.ru>
2024-03-12 13:58:57 +0500

    add config

---
+++ src/app/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './styles/index.scss'
+import { Link } from 'react-router-dom'
+
+import { useTheme } from 'app/providers/ThemeProvider'
+import { classNames } from 'shared/lib/classNames/classNames'
+import { AppRouter } from 'app/providers/router'
+
+const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>toggle</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+
+            <AppRouter />
+
+        </div>
+    )
+}
+
+export default App
