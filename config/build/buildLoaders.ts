9c726cd7c0a94692b38df8f53d4bc06e1eb3938b
a.galeev <a.galeev@malltech-lab.ru>
2024-03-13 15:59:46 +0500

    add Sidebar

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,20 @@ import { BuildOptions } from './types/config'
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -29,5 +43,10 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
-    return [ tsLoader, cssLoader ]
+    return [
+        fileLoader,
+        svgLoader,
+        tsLoader,
+        cssLoader
+    ]
 }
