e271a27c6330f2285ce04501a2847ace81eba139
a.galeev <a.galeev@malltech-lab.ru>
2024-03-14 22:37:29 +0500

    add ErrorBoundary

---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { classNames } from 'shared/lib/classNames/classNames'
+import './Loader.scss'
+
+interface LoaderProps {
+    className?: string
+}
+
+export const Loader = ({className}: LoaderProps) => {
+    return (
+        <div className={classNames('lds-dual-ring', {}, [className])}>
+        </div>
+    )
+}
