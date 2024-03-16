2cfd8d3167e3028641aca06487a62c50f592da29
a.galeev <a.galeev@malltech-lab.ru>
2024-03-16 12:24:40 +0500

    add checks

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -11,19 +11,19 @@ interface SidebarProps {
 export const Sidebar = ({className}: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false)
 
-    const onToggle = () => {
-        setCollapsed(prev => !prev)
-    }
+    // const onToggle = () => {
+    //     setCollapsed(prev => !prev)
+    // }
 
     return (
         <div
             data-testid="sidebar"
             className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed }, [className])}
         >
-            <button
-                data-testid="sidebar-toggle"
-                onClick={onToggle}
-            >toggle</button>
+            {/*<button*/}
+            {/*    data-testid="sidebar-toggle"*/}
+            {/*    onClick={onToggle}*/}
+            {/*>toggle</button>*/}
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
